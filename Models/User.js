78c00a07476511e6d1e8274cebe3f6302e612bf2
Fuad Abou Heit <f.abouheit@gmail.com>
2023-11-21 10:58:26 +0200

    created models user and notifications

--- Models/User.js
+++ Models/User.js
@@ -1,1 +1,46 @@
 // User Model
+
+import { ENUM, INTEGER, STRING } from "sequelize";
+import sequelize from "../config/config.js";
+
+const User = sequelize.define('User', {
+
+    idUser : {
+        type: INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        field: 'id_user',
+    },
+    userName :{
+        type: STRING({length:256}),
+        field: 'user_name',
+        unique: true,
+        allowNull: false
+    },
+    name : {
+        type: STRING({length:256}),
+        unique: true,
+        allowNull: false
+    },
+    email : {
+        type: STRING({length:256}),
+        unique: true,
+        allowNull: false
+    },
+    password : {
+        type: STRING,
+        allowNull: false,
+    },
+    role : {
+        type: ENUM(['admin', 'user','merchant']),
+        defaultValue: 'user',
+        allowNull:false,
+
+    },
+    avatar : {
+        type: STRING,
+        defaultValue: "",
+    }
+}, {sequelize, createdAt: 'created_at', updatedAt: 'updated_at'} )
+
+export default User;
